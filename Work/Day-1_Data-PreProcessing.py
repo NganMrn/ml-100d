a9e321129ea8c7b6f6e79ac4542e37a61a10f786
Ngan Tran <tranvanngan@kng.vn>
2020-02-03 13:50:18 +0700

    update function for model_selection

--- Work/Day-1_Data-PreProcessing.py
+++ Work/Day-1_Data-PreProcessing.py
@@ -16,7 +16,6 @@ print("app running ------------------------------------------------")
 ## Step 3: Handling the missing data
 from sklearn.impute import SimpleImputer 
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-
 imputer = imputer.fit(X[ : , 1:3])
 X[ : , 1:3] = imputer.transform(X[ : , 1:3])
 
